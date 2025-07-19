8203e12f4a50f4857d19b3732c49733572b183fd
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-07-19 10:59:51 +0530

    feat: implement custom font loading and update text styling in layout and index components

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-5xl text-center
+       font-bold text-blue-500 font-quicksand-bold">
         Welcome to Nativewind!
       </Text>
     </View>
