f0010784bfeeb545bb163a0647e6bc645017d886
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-07-21 10:46:49 +0530

    feat: update Appwrite configuration to include database and user collection IDs

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,5 +2,7 @@
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform: "com.smruti.food",
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId:process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    userCollectionId:process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID
 }
