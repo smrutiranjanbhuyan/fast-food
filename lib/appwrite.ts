3a2219db45b821572551c8777e1cf3340f523543
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-07-21 10:32:29 +0530

    feat: add Appwrite configuration and dependencies for backend integration

---
+++ lib/appwrite.ts
@@ -0,0 +1,6 @@
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.smruti.food",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
