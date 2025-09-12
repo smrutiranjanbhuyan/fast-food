73cf8bb2817bfdbc55247b11bc6292463d26ceb4
SMRUTI RANJAN BHUYAN <smruti452004@gmail.com>
2025-09-12 21:19:03 +0530

    feat: update CartButton to handle navigation and improve structure

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,13 +1,15 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
 import { images } from '@/constants';
 import { useCartStore } from '@/store/cart.store';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 const CartButton = () => {
+  const router = useRouter();
  const {getTotalItems} = useCartStore();
   const totalItems = getTotalItems();
   return (
-    <TouchableOpacity className='cart-btn' onPress={() => { }}>
+    <TouchableOpacity className='cart-btn' onPress={() => router.push('/cart')}>
       <Image source={images.bag} className={'size-5'} resizeMode='contain' />
       {
         totalItems > 0 && (
